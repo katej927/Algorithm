495faf05be4e02672dfc8c83860b3bd336152b06
Kate Jung <69146527+katej927@users.noreply.github.com>
2021-04-03 16:29:04 +0900

    Update March.js

--- March.js
+++ March.js
@@ -168,3 +168,13 @@ var numberOfMatches = function(n) {
   return collectResult
 
 };
+
+/* ['21.4.3] leetCode 1480 */
+let runningSum = function(nums) {
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        sum=sum+nums[i];
+        nums[i]=sum;
+    }
+    return nums;
+};
