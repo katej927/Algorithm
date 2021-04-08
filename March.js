2e961793fd9862002a841b161240205c125615f0
정선미 Kate Seonmi Jung <kmsun0720@naver.com>
2021-04-08 17:39:46 +0900

    '21.4.8 알고리즘

--- March.js
+++ March.js
@@ -1,67 +1,32 @@
   /* ['21.3.21] 프로그래머스_주식가격 */
-
-  public static int[] solution2(int[] prices) {
-		int[] answer = new int[prices.length];
-
-		for(int i = 0; i < prices.length; i++) {
-			for(int j=i+1; j < prices.length; j++){
-				answer[i] = j-i;
-				if(prices[i] > prices[j])  break;
-			}
-		}
-
-		return answer;
-	}
+  function solution(prices) {
+    let answer = new Array(prices.length).fill(0);
+    let stack = [];
+    let length = prices.length;
+    for(let i = 0; i < length; i++) {
+        while(stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+            let temp = stack.pop();
+            answer[temp] = i - temp;
+        }
+        stack.push(i)
+    }
+    while(stack.length) {
+        let temp = stack.pop();
+        answer[temp] = length - temp - 1;
+    }
+    return answer;
+}
 
 
 /* ['21.3.22] 프로그래머스_소수 찾기 */
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <cmath>
-using namespace std;
-
-unordered_set <int> s;
-string str = "";
-bool check[7];
-
-void find_all_numbers(int depth, int limit, string& numbers){
-    if(depth == limit) return;
-    
-    for(int i = 0 ; i < limit ; i++){
-        if(!check[i]){
-            check[i] = true;
-            str.push_back(numbers[i]);
-            s.insert(stoi(str));
-            find_all_numbers(depth+1, limit, numbers);
-            str.pop_back();
-            check[i] = false;
-        }
+function solution(n) {
+    const 소수들 = [2];
+    for (let i = 2; i <= n; i++) {
+      const is소수 = 소수들.some((c) => i % c === 0);
+      if (!is소수) 소수들.push(i);
     }
-}
-int solution(string numbers) {
-    int size = numbers.size();
-    
-    find_all_numbers(0,size, numbers);
-    
-    int answer = 0;
-    
-    for(auto& it : s){
-        if(it == 1 || it == 0) continue;
-        
-        int num = sqrt(it);
-        // sqrt를 사용하지 않고, 
-        // for(int i = 2 ; i*i <= it ; i++){ }을 쓰는 방법도 있다.
-        bool isDivided = false;
-        for(int i = 2 ; i <= num ; i++){
-            if(it % i == 0) isDivided = true;
-        }
-        if(!isDivided) answer++;
-    }
-    
-    return answer;
-}
+    return 소수들.length;
+  }
 
 /* ['21.3.24] Programmers level 1_ Deduplication*/
 function solution(arr) {
