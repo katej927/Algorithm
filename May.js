c911d3d18f06b9ae86298f6d70996f265d36d493
정선미 Kate Seonmi Jung <kmsun0720@naver.com>
2021-05-06 22:05:59 +0900

    Add: 알고리즘

--- May.js
+++ May.js
@@ -9,7 +9,7 @@ var checkIfPangram = function (sentence) {
   return a.length == 26 ? true : false;
 };
 
-/* ['21.5.2] leetCode 1486 */
+/* ['21.5.3] leetCode 1486 */
 var xorOperation = function (n, start) {
   const result = new Array(n)
     .fill(0)
@@ -18,9 +18,30 @@ var xorOperation = function (n, start) {
   return result;
 };
 
-/* ['21.5.2] leetCode 938 */
+/* ['21.5.5] leetCode 938 */
 var rangeSumBST = function (root, L, R) {
   let d = (r) =>
     !r ? 0 : (L <= r.val && r.val <= R ? r.val : 0) + d(r.left) + d(r.right);
   return d(root);
+};
+
+/* ['21.5.6] leetCode 1844 */
+var replaceDigits = function (s) {
+  const chs = s.split("");
+  const len = chs.length;
+
+  for (let i = 1; i < len; i += 2) {
+    const prevCh = chs[-1 + i];
+    let chCode = s.charCodeAt(-1 + i);
+    let ch = s[i];
+    const offset = Number(ch);
+
+    chCode += offset;
+
+    ch = String.fromCharCode(chCode);
+    chs[i] = ch;
+  }
+
+  let result = chs.join("");
+  return result;
 };
