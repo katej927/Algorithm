957f1dbcf6cce9c25525ee8ce1624a79c906a4c6
정선미 Kate Seonmi Jung <kmsun0720@naver.com>
2021-05-11 23:27:41 +0900

    Add: 알고리즘

--- May.js
+++ May.js
@@ -46,17 +46,38 @@ var replaceDigits = function (s) {
   return result;
 };
 
-/* ['21.5.8] leetCode 1662 */
+/* ['21.5.9] leetCode 1662 */
 var arrayStringsAreEqual = function (word1, word2) {
   const result = word1.join("") === word2.join("") ? true : false;
   return result;
 };
 
-/* ['21.5.8] leetCode 1290 */
-let result = 0
-    while (head) {
-        result = result * 2 + head.val;
-        head = head.next
+/* ['21.5.10] leetCode 1290 */
+var getDecimalValue = function (head, result) {
+  if (!result) {
+    result = "";
+  }
+  result = result + String(head.val);
+  if (!head.next) {
+    return parseInt(result, 2);
+  }
+  return getDecimalValue(head.next, result);
+};
+
+/* ['21.5.11] leetCode 1290 */
+var countGoodTriplets = function (arr, a, b, c) {
+  const result = [];
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        const absCondition1 = Math.abs(arr[i] - arr[j]) <= a;
+        const absCondition2 = Math.abs(arr[j] - arr[k]) <= b;
+        const absCondition3 = Math.abs(arr[i] - arr[k]) <= c;
+        if (absCondition1 && absCondition2 && absCondition3) {
+          result.push([arr[i], arr[j], arr[k]]);
+        }
+      }
     }
-    return result
+  }
+  return result.length;
 };
