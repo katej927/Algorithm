56b1e651fa3b477af3b018cdfeae40b5da14f25d
정선미 <kmsun0720@naver.com>
2021-05-27 22:58:22 +0900

    Add: 알고리즘

--- May.js
+++ May.js
@@ -256,8 +256,47 @@ var diagonalSum = function (arr) {
   let answer = 0;
   const arrLen = arr.length;
   const arrHalf = Math.floor(arrLen / 2);
-  for (let = 0; i < arrLen; i++) {
+  for (let i = 0; i < arrLen; i++) {
     answer += arr[i][i] + arr[i][arrLen - 1 - i];
   }
   return arrLen % 2 === 1 ? answer - arr[arrHalf][arrHalf] : answer;
+};
+
+/* ['21.5.27] leetCode 1309 */
+var freqAlphabets = function (s) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let number = "12346789";
+  let digitNumber = [
+    "10#",
+    "11#",
+    "12#",
+    "13#",
+    "14#",
+    "15#",
+    "16#",
+    "17#",
+    "18#",
+    "19#",
+    "20#",
+    "21#",
+    "22#",
+    "23#",
+    "24#",
+    "25#",
+    "26#",
+  ];
+
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 2] === "#") {
+      let digit = "";
+      digit += s[i] + s[i + 1] + s[i + 2];
+      let index = digitNumber.indexOf(digit) + 9;
+      answer += alphabet[index];
+      i += 2;
+    } else if (s[i] >= 1 && s[i] <= 9) {
+      answer += alphabet[s[i] - 1];
+    }
+  }
+  return answer;
 };
