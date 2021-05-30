aafb98961a6f199d0cd067f9d78be1b2a790fdee
정선미 <kmsun0720@naver.com>
2021-05-31 00:08:48 +0900

    Add: 알고리즘

--- May.js
+++ May.js
@@ -375,3 +375,12 @@ var maxProduct = function (nums) {
   }
   return result;
 };
+
+/* ['21.5.31] leetCode 1374 */
+var generateTheString = function (n) {
+  if (n % 2 === 0) {
+    return "a".repeat(n - 1) + "b";
+  } else {
+    return "a".repeat(n);
+  }
+};
