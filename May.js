f4bc33f7daf6247b63734d77b3495b7a0eefb7d0
정선미 <kmsun0720@naver.com>
2021-06-01 00:09:24 +0900

    Add: 알고리즘

--- May.js
+++ May.js
@@ -384,3 +384,17 @@ var generateTheString = function (n) {
     return "a".repeat(n);
   }
 };
+
+/* ['21.6.1] leetCode 1450 */
+var busyStudent = function (startTime, endTime, queryTime) {
+  const timeLength = startTime.length;
+  let busyStudentCount = 0;
+
+  for (let i = 0; i < timeLength; i++) {
+    if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+      busyStudentCount++;
+    }
+  }
+
+  return busyStudentCount;
+};
