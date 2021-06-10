58b42f67dce52be4ae961922ee8394b7fe48a0f7
정선미 <kmsun0720@naver.com>
2021-06-10 21:19:53 +0900

    Add: 알고리즘

--- June.js
+++ June.js
@@ -54,10 +54,23 @@ var selfDividingNumbers = function (left, right) {
 
 /* ['21.6.8] leetCode 617 */
 var mergeTrees = function (t1, t2) {
+  //
   if (t1 === null) return t2;
   if (t2 === null) return t1;
   t1.val += t2.val;
   t1.left = mergeTrees(t1.left, t2.left);
   t1.right = mergeTrees(t1.right, t2.right);
   return t1;
+};
+
+/* ['21.6.10] leetCode 1351 */
+var countNegatives = function (grid) {
+  let result = 0;
+  for (let i of grid) {
+    for (let j = i.length - 1; j >= 0; j--) {
+      if (i[j] < 0) result++;
+      if (i[j] >= 0) break;
+    }
+  }
+  return result;
 };
