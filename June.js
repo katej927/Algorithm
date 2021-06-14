253d30ef22deffc695f40a0b54882ebeec611782
정선미 <kmsun0720@naver.com>
2021-06-14 22:01:02 +0900

    Add: 알고리즘

--- June.js
+++ June.js
@@ -115,3 +115,18 @@ var increasingBST = function (root) {
 
   return newRoot.right;
 };
+
+/* ['21.6.14] leetCode 961 */
+var repeatedNTimes = function (A) {
+  var expectedRepeatingTImes = A.length;
+  var arrayCountObj = {};
+  for (var i = 0; i < expectedRepeatingTImes; i++) {
+    if (!arrayCountObj["arr_" + A[i]]) {
+      arrayCountObj["arr_" + A[i]] = 0;
+    }
+    arrayCountObj["arr_" + A[i]] = arrayCountObj["arr_" + A[i]] + 1;
+    if (arrayCountObj["arr_" + A[i]] == expectedRepeatingTImes / 2) {
+      return A[i];
+    }
+  }
+};
