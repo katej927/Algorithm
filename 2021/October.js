4e3821e7942e2e7eda6009e3276cedb4501ee801
정선미 <kmsun0720@naver.com>
2021-10-02 15:01:48 +0900

    leetcode 1512

---
+++ 2021/October.js
@@ -0,0 +1,15 @@
+// ['21.10.2] leetcode 1512
+const nums = [1, 2, 3];
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+numIdenticalPairs(nums);
