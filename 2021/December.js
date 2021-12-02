0ace07758a6100c5398129ab154b765a3c96adbb
정 선 미 <kmsun0720@naver.com>
2021-12-02 12:28:32 +0900

    leetcode 1588

--- 2021/December.js
+++ 2021/December.js
@@ -4,3 +4,21 @@ var targetIndices = function (nums, target) {
     .sort((a, b) => a - b)
     .reduce((acc, cur, idx) => (cur === target ? [...acc, idx] : acc), []);
 };
+
+// ['21.12.2] leetcode 1588
+var sumOddLengthSubarrays = function (arr) {
+  let addIdx = 1,
+    resultArr = [];
+  let replay = (addIdx) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr.slice(i, i + addIdx).length === addIdx) {
+        resultArr.push(arr.slice(i, i + addIdx));
+      }
+    }
+  };
+  while (addIdx <= arr.length) {
+    replay(addIdx);
+    addIdx += 2;
+  }
+  return resultArr.flat().reduce((a, c) => a + c, 0);
+};
