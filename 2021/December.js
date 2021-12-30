b4af37fc6022e0bdb170dcfc6ae5f20a210a04f3
Seonmi Jung <devkatej927@gmail.com>
2021-12-30 22:02:56 +0900

    Refactor: leetcode 1534

--- 2021/December.js
+++ 2021/December.js
@@ -209,12 +209,10 @@ var countGoodTriplets = function (arr, a, b, c) {
   let count = 0;
   for (var i = 0; i < arr.length - 2; i++) {
     for (var j = i + 1; j < arr.length - 1; j++) {
+      if (Math.abs(arr[i] - arr[j]) > a) continue;
       for (var k = j + 1; k < arr.length; k++) {
-        let condition =
-          Math.abs(arr[i] - arr[j]) <= a &&
-          Math.abs(arr[j] - arr[k]) <= b &&
-          Math.abs(arr[i] - arr[k]) <= c;
-        condition ? count++ : count;
+        if (Math.abs(arr[j] - arr[k]) > b) continue;
+        if (Math.abs(arr[i] - arr[k]) <= c) count++;
       }
     }
   }
