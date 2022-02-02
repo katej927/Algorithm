52120eefd4e553f14ebdd5ded0d9d98892f35324
Seonmi Jung <devkatej927@gmail.com>
2022-02-02 09:07:15 +0900

    // ['22.2.2] Programmers H-Index

---
+++ 2022/February.js
@@ -0,0 +1,7 @@
+// ['22.2.2] programmers H-Index
+function solution(citations) {
+  for (let h = Math.max(...citations); h >= 0; h--) {
+    if (citations.filter((citation) => citation >= h).length >= h) return h;
+  }
+  return 0;
+}
