8d1adc03e22ab58dd755fbbbe29abcd84699bebd
Seonmi Jung <devkatej927@gmail.com>
2022-02-14 10:11:43 +0900

    BFS 구현 연습

--- 2022/February.js
+++ 2022/February.js
@@ -60,3 +60,39 @@ function solution(bridge_length, weight, truck_weights) {
   return time;
 }
 solution(2, 10, [7, 4, 5, 6]);
+
+// ['22.2.14] BFS 구현 연습
+
+const graph = {
+  A: ["B", "C"],
+  B: ["A", "D"],
+  C: ["A", "G", "H", "I"],
+  D: ["B", "E", "F"],
+  E: ["D"],
+  F: ["D"],
+  G: ["C"],
+  H: ["C"],
+  I: ["C", "J"],
+  J: ["I"],
+};
+
+const BFS = (graph, startNode) => {
+  const visited = []; // 탐색을 마친 노드들
+  let needVisit = []; // 탐색해야할 노드들
+
+  needVisit.push(startNode); // 노드 탐색 시작
+
+  while (needVisit.length) {
+    // 탐색해야할 노드가 남아있다면
+    const node = needVisit.shift(); // queue이기 때문에 선입선출, shift()를 사용한다.
+    if (!visited.includes(node)) {
+      // 해당 노드가 탐색된 적 없다면
+      visited.push(node);
+      needVisit = [...needVisit, ...graph[node]];
+    }
+  }
+  return visited;
+};
+
+console.log(BFS(graph, "A"));
+// ["A", "B", "C", "D", "G", "H", "I", "E", "F", "J"]
