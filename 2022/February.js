29c137f06ee3070b084c6a6502ff202382e4e7c5
Seonmi Jung <devkatej927@gmail.com>
2022-02-16 15:33:58 +0900

    DFS 구현 연습

--- 2022/February.js
+++ 2022/February.js
@@ -96,3 +96,24 @@ const BFS = (graph, startNode) => {
 
 console.log(BFS(graph, "A"));
 // ["A", "B", "C", "D", "G", "H", "I", "E", "F", "J"]
+
+// ['22.2.16] DFS 구현 연습
+const DFS = (graph, startNode) => {
+  const visited = []; // 탐색을 마친 노드들
+  let needVisit = []; // 탐색해야할 노드들
+
+  needVisit.push(startNode); // 노드 탐색 시작
+
+  while (needVisit.length) {
+    // 탐색해야할 노드가 남아있다면
+    const node = needVisit.shift(); // queue이기 때문에 선입선출, shift()를 사용한다.
+    if (!visited.includes(node)) {
+      // 해당 노드가 탐색된 적 없다면
+      visited.push(node);
+      needVisit = [...graph[node], ...needVisit];
+    }
+  }
+  return visited;
+};
+console.log(DFS(graph, "A"));
+// ["A", "B", "D", "E", "F", "C", "G", "H", "I", "J"]
