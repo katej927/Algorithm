145710c2acf9d8c68b614e2cc1e3902fb2a3d49c
Seonmi Jung <devkatej927@gmail.com>
2022-03-06 16:41:17 +0900

    Refactor: leetcode 2185

--- 2022/March.js
+++ 2022/March.js
@@ -1,4 +1,13 @@
 // ['22.3.6] leetcode 2185
+// ver 1
 var prefixCount = function (words, pref) {
   return words.reduce((acc, cur) => (cur.indexOf(pref) ? acc : (acc += 1)), 0);
+};
+
+// ver 2
+var prefixCount = function (words, pref) {
+  return words.reduce(
+    (acc, cur) => (cur.startsWith(pref) ? (acc += 1) : acc),
+    0
+  );
 };
