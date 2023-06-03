bc0d133d5aa3255b5b6c1c003a515bbe7f719bce
Seonmi Jung <devkatej927@gmail.com>
2023-06-03 11:56:51 +0900

    Inflearn : 자바스크립트 알고리즘 문제풀이 입문_8. 재귀함수와 완전탐색(DFS:깊이우선탐색)_2. 재귀함수를 이용한 이진수 출력 (solution 2)

--- 2023/June.ts
+++ 2023/June.ts
@@ -1,10 +1,26 @@
 // ['23.6.3] Inflearn : 자바스크립트 알고리즘 문제풀이 입문_8. 재귀함수와 완전탐색(DFS:깊이우선탐색)_2. 재귀함수를 이용한 이진수 출력
 // solution 1
-function convertToBinary(n: number) {
+function convertToBinary1(n: number) {
   const iterate: (quotient: number, remainder: string) => string = (quotient: number, remainder: string) =>
     quotient < 2 ? `${quotient}${remainder}` : iterate(Math.floor(quotient / 2), `${quotient % 2}${remainder}`)
 
   return iterate(n, '')
 }
 
-convertToBinary(11)
+convertToBinary1(11)
+
+// solution 2
+function convertToBinary2(n: number) {
+  let answer: string = ''
+  const iterate = (n: number) => {
+    if (n === 0) return
+    else {
+      iterate(Math.floor(n / 2))
+      answer += n % 2
+    }
+  }
+  iterate(n)
+  return answer
+}
+
+convertToBinary2(11)
