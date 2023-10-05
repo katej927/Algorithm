ab8d3775dc503f8fbf45e9c41103ab8560deb3bd
Seonmi Jung <devkatej927@gmail.com>
2023-10-05 10:43:57 +0900

    proceeding: leetcode 322. Coin Change

--- 2023/October.ts
+++ 2023/October.ts
@@ -49,3 +49,51 @@ const TESTS1 = [
     console.log(`${i}번째 case: ${isSuccess ? 'SUCCESS' : 'FAILURE'}`)
   }
 })()
+
+// ['23.10.5] leetcode 322. Coin Change
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  if (!amount) return 0
+  const sortedCoins = coins.sort((a, b) => b - a)
+}
+
+const TEST_231005 = [
+  {
+    params: {
+      coins: [1, 2, 5],
+      amount: 11,
+    },
+    result: 3,
+  },
+  {
+    params: {
+      coins: [2],
+      amount: 3,
+    },
+    result: -1,
+  },
+  {
+    params: {
+      coins: [1],
+      amount: 0,
+    },
+    result: 0,
+  },
+]
+
+;(function () {
+  for (let i = 0; i < TEST_231005.length; i++) {
+    const {
+      params: { coins, amount },
+      result,
+    } = TEST_231005[i]
+    const res = coinChange(coins, amount)
+    const isSuccess = res === result
+
+    console.log(`${i}번째 case: ${isSuccess ? 'SUCCESS' : 'FAILURE'}`)
+  }
+})()
