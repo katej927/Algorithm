2ce841e05a6c02e20cfd655474ee860d505a9200
Seonmi Jung <devkatej927@gmail.com>
2023-12-09 09:58:01 +0900

    baekjoon 1000. A+B

--- 2023/December.ts
+++ 2023/December.ts
@@ -6,3 +6,9 @@
 var minimizedStringLength = function (s) {
   return new Set(s).size
 }
+
+// ['23.12.9] baekjoon 1000. A+B
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split(' ')
+
+console.log(input.reduce((acc, cur) => acc + Number(cur), 0))
