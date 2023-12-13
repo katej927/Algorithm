c1409cfe020c7a1a09334f727d502a4ec6019601
Seonmi Jung <devkatej927@gmail.com>
2023-12-13 10:46:50 +0900

    baekjoon 10998. A×B

--- 2023/December.ts
+++ 2023/December.ts
@@ -12,3 +12,14 @@ let fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().split(' ')
 
 console.log(input.reduce((acc, cur) => acc + Number(cur), 0))
+
+// ['23.12.13] baekjoon 10998. A×B
+const fs2 = require('fs')
+const input2 = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+const line = input[0].split(' ')
+
+const a = parseInt(line[0])
+const b = parseInt(line[1])
+
+console.log(a * b)
