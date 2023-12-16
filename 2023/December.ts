5fb5c857e48b32a0f41029f14363f6ea4a0403b5
Seonmi Jung <devkatej927@gmail.com>
2023-12-16 13:48:30 +0900

    baekjoon 2588. 시험 성적

--- 2023/December.ts
+++ 2023/December.ts
@@ -9,15 +9,15 @@ var minimizedStringLength = function (s) {
 
 // ['23.12.9] baekjoon 1000. A+B
 let fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split(' ')
+let input1 = fs.readFileSync('/dev/stdin').toString().split(' ')
 
-console.log(input.reduce((acc, cur) => acc + Number(cur), 0))
+console.log(input1.reduce((acc, cur) => acc + Number(cur), 0))
 
 // ['23.12.13] baekjoon 10998. A×B
 const fs2 = require('fs')
 const input2 = fs.readFileSync('/dev/stdin').toString().split('\n')
 
-const line = input[0].split(' ')
+const line = input2[0].split(' ')
 
 const a = parseInt(line[0])
 const b = parseInt(line[1])
@@ -38,3 +38,25 @@ for (const secNum of splitedSecondNums) {
 }
 
 console.log(firstNum * Number(secondNums))
+
+// ['23.12.14] baekjoon 2588. 시험 성적
+const fs4 = require('fs')
+const input4 = fs4.readFileSync('/dev/stdin').toString().split('\n')
+
+const score = Number(input4[0])
+
+const convertToGrade = (line) => {
+  if (90 <= line && line <= 100) {
+    console.log('A')
+  } else if (80 <= line && line <= 89) {
+    console.log('B')
+  } else if (70 <= line && line <= 79) {
+    console.log('C')
+  } else if (60 <= line && line <= 69) {
+    console.log('D')
+  } else {
+    console.log('F')
+  }
+}
+
+convertToGrade(score)
