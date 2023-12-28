457a1c7ed4dad034ac181cb054189ce0cbf2d81f
Seonmi Jung <devkatej927@gmail.com>
2023-12-28 09:46:18 +0900

    baekjoon 8393. 합

--- 2023/December.ts
+++ 2023/December.ts
@@ -101,3 +101,11 @@ else if (first === second) console.log(1000 + first * 100)
 else if (first === third) console.log(1000 + first * 100)
 else if (second === third) console.log(1000 + second * 100)
 else console.log(Math.max(first, second, third) * 100)
+
+// ['23.12.28] baekjoon 8393. 합
+const fs8 = require('fs')
+const input8 = fs8.readFileSync('/dev/stdin').toString()
+
+const limit = Number(input8)
+
+console.log([...Array(limit + 1).keys()].reduce((acc, cur) => acc + cur, 0))
