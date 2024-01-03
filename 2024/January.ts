019d4631cdb27fcc7993beb0a4261ba5b275e2d1
Seonmi Jung <devkatej927@gmail.com>
2024-01-03 10:48:09 +0900

    baekjoon 2438. 별 찍기 - 1

---
+++ 2024/January.ts
@@ -0,0 +1,13 @@
+// ['23.1.3] baekjoon 2438. 별 찍기 - 1
+
+const fs1 = require('fs')
+const input = fs1.readFileSync('/dev/stdin').toString()
+
+const inputToNum = Number(input)
+
+let result = ''
+for (let i = 1; i <= inputToNum; i++) {
+  result += '*'.repeat(i) + '\n'
+}
+
+console.log(result)
