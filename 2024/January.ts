28012bb77cd8f65a643b33a796c3f2872f226317
Seonmi Jung <devkatej927@gmail.com>
2024-01-04 10:05:36 +0900

    baekjoon 15552. 빠른 A+B

--- 2024/January.ts
+++ 2024/January.ts
@@ -11,3 +11,15 @@ for (let i = 1; i <= inputToNum; i++) {
 }
 
 console.log(result)
+
+// ['23.1.4] baekjoon 15552. 빠른 A+B
+const fs240104 = require('fs')
+const input240104 = fs240104.readFileSync('input.txt').toString().split('\n')
+
+let result5 = ''
+for (let i = 1; i <= Number(input240104[0]); i++) {
+  const [firstNum, secondNum] = input240104[i].split(' ')
+  result5 += Number(firstNum) + Number(secondNum) + '\n'
+}
+
+console.log(result5)
