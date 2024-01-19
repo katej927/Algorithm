d8e37420de054fc2bd75fa95d179293892e10f09
Seonmi Jung <devkatej927@gmail.com>
2024-01-19 20:07:20 +0900

    refactor : baekjoon 2675. 문자열 반복

--- 2024/January.ts
+++ 2024/January.ts
@@ -118,17 +118,17 @@ const total = numsInString.reduce((acc, cur) => {
 console.log(total)
 
 // ['24.1.17] baekjoon 2675. 문자열 반복
-const fs2401172 = require('fs')
-const input2401172 = fs2401172.readFileSync('input.txt').toString()
+const fs240119 = require('fs')
+const input240119 = fs240119.readFileSync('input.txt', 'utf8').trim().split('\n')
 
-let result2401172 = ''
+const targets = input240119.slice(1)
 
-const targets = input2401172.slice(1).trim().split('\n')
-for (let target of targets) {
-  for (let idx = 2; idx < target.length; idx++) {
-    const repeatedLetters = target[idx].repeat(target[0])
-    result2401172 += repeatedLetters
+targets.forEach((target) => {
+  const [loopNum, letters] = target.split(' ')
+
+  let eachResult = ''
+  for (let letter of letters) {
+    eachResult += letter.repeat(loopNum)
   }
-  console.log(result2401172)
-  result2401172 = ''
-}
+  console.log(eachResult)
+})
