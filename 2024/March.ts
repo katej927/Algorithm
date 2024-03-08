6864d9d5a22d384e438310a6083618acbe3bcfe6
Seonmi Jung <devkatej927@gmail.com>
2024-03-08 10:42:22 +0900

    refactoring : baekjoon 11047. 동전 0

--- 2024/March.ts
+++ 2024/March.ts
@@ -2,21 +2,15 @@
 const fs3 = require('fs')
 const input3 = fs3.readFileSync('input.txt', 'utf8').trim().split('\n')
 
-const [coinsCount, total] = input3[0].split(' ')
-const coinsToNumber = input3.slice(1).map(Number)
+let [coinsCount, total] = input3[0].split(' ')
+const coinsToNumber = input3.slice(1).map(Number).reverse()
 
-let portions = Array(Number(coinsCount)).fill(0)
+const result = coinsToNumber.reduce((acc, cur) => {
+  if (cur > total) return acc
 
-for (let i = coinsToNumber.length - 1; i >= 0; i--) {
-  const currentCountingCoin = coinsToNumber[i]
-  let portion = 0
+  acc += Math.floor(total / cur)
+  total %= cur
+  return acc
+}, 0)
 
-  if (currentCountingCoin > total) {
-    continue
-  }
-  for (let j = i; j >= 0; j--) {
-    if (total % coinsToNumber[j] === 0) {
-      portions[i] += Math.floor(total / coinsToNumber[j])
-    }
-  }
-}
+console.log(result)
