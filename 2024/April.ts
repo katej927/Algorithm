d01f9470e48dd1f41b519b16049ac5a4ce9555b6
Seonmi Jung <devkatej927@gmail.com>
2024-04-08 14:53:58 +0900

    proceeding : baekjoon 13305. 주유소

--- 2024/April.ts
+++ 2024/April.ts
@@ -1,3 +1,18 @@
 // ['24.4.6] baekjoon 13305. 주유소
 const fs6 = require('fs')
 const input6 = fs6.readFileSync('input.txt', 'utf8').trim()
+
+const distance = input6[1].split(' ').map(Number)
+const price = input6[2].split(' ').slice(0, -1).map(Number)
+const sortedPrice = [...price].sort((a, b) => b - a)
+
+let curMinPriceIdx = 0
+
+console.log(
+  price.reduce((totalPrice, curPrice, idx) => {
+    const curMinPrice = sortedPrice[curMinPriceIdx]
+    if (curMinPrice > curPrice) {
+      curMinPriceIdx = sortedPrice.findIndex((price) => price === curPrice)
+    }
+  }, 0)
+)
