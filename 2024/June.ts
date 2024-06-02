1cca8b7583a4090b00dbe0fbd1caaba7e7ff25f0
Seonmi Jung <devkatej927@gmail.com>
2024-06-02 11:20:42 +0900

    proceeding : baekjoon 1493. 박스 채우기

--- 2024/June.ts
+++ 2024/June.ts
@@ -1,3 +1,10 @@
 // ['24.6.1] baekjoon 1493. 박스 채우기
 const fs1 = require('fs')
-const input1 = fs1.readFileSync('input.txt', 'utf8').trim()
+const input1 = fs1.readFileSync('input.txt', 'utf8').trim().split('\n')
+
+const [length1, width, height] = input1[0].split(' ')
+const boxSize = length1 * width * height
+
+let count = 0
+
+const cubeSizes = input1.slice(2).reverse()
