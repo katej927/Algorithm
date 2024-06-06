5efe7f2c53c3cea9b503d7aed280ef020c92ce2c
Seonmi Jung <devkatej927@gmail.com>
2024-06-06 22:56:23 +0900

    proceeding : baekjoon 1493. 박스 채우기

--- 2024/June.ts
+++ 2024/June.ts
@@ -1,29 +1,8 @@
 // ['24.6.1] baekjoon 1493. 박스 채우기
-const fs1 = require('fs')
-const input1 = fs1.readFileSync('input.txt', 'utf8').trim().split('\n')
+let fs = require('fs')
+let input = fs.readFileSync('input.txt').toString().split('\n')
 
-const [length1, width, height] = input1[0].split(' ')
-const boxSize = length1 * width * height
+const [boxSize, NumberOfCubeType] = input
 
-let leftedSize = boxSize
-let result = 0
-
-const cubeSizes = input1
-  .slice(2)
-  .map((el) => el.split(' '))
-  .reverse()
-
-for (let [size, count] of cubeSizes) {
-  count = Number(count)
-
-  const currentCubeSize = (2 ** size) ** 3
-  const dividedCount = Math.floor(leftedSize / currentCubeSize)
-  const usedCurrentCubeCount = Math.min(dividedCount, count)
-
-  result += usedCurrentCubeCount
-
-  leftedSize = leftedSize - currentCubeSize * usedCurrentCubeCount
-
-  if (leftedSize === 0) break
-}
-console.log('result', leftedSize ? -1 : result)
+let [length1, width, height] = boxSize.split(' ').map(Number)
+const cubeList = input.slice(2)
