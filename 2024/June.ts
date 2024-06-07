8d9cbfd530978bb557295fb8ab0b9388968e8ae5
Seonmi Jung <devkatej927@gmail.com>
2024-06-07 10:39:08 +0900

    proceeding : baekjoon 1493. 박스 채우기

--- 2024/June.ts
+++ 2024/June.ts
@@ -5,4 +5,17 @@ let input = fs.readFileSync('input.txt').toString().split('\n')
 const [boxSize, NumberOfCubeType] = input
 
 let [length1, width, height] = boxSize.split(' ').map(Number)
+
 const cubeList = input.slice(2)
+const cubes = cubeList.map((cubeInfo) => {
+  const [, currentCubeCount] = cubeInfo.split(' ')
+  return Number(currentCubeCount)
+})
+
+function nearestSquare(x) {
+  let i = 1
+  while (2 ** i <= x) ++i
+  return i - 1
+}
+
+let size = Math.min(nearestSquare(length1), nearestSquare(width), nearestSquare(height))
