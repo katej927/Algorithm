13714b96c0f22bd59b51214b537c485cc6b0b376
Seonmi Jung <devkatej927@gmail.com>
2024-06-14 12:42:42 +0900

    fast campus. 이진탐색 재귀함수 예시

--- 2024/June.ts
+++ 2024/June.ts
@@ -38,3 +38,15 @@ for (let i = size; i >= 0; i--) {
 }
 
 console.log(usedVolume === boxSize ? countOfUsedCube : -1)
+
+// ['24.6.14] fast campus. 이진탐색 재귀함수 예시
+function binarySearch(arr, target, start, end) {
+  if (start > end) return -1
+  let mid = parseInt(String((start + end) / 2))
+  // 찾은 경우 중간점 인덱스 반환
+  if (arr[mid] == target) return mid
+  // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+  else if (arr[mid] > target) return binarySearch(arr, target, start, mid - 1)
+  // 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+  else return binarySearch(arr, target, mid + 1, end)
+}
