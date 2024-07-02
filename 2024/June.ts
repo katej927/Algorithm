ec9f26496f640f580fb002446d7611550f1e01dd
Seonmi Jung <devkatej927@gmail.com>
2024-07-02 09:14:41 +0900

    proceeding : fast campus. 파라메트릭 서치

--- 2024/June.ts
+++ 2024/June.ts
@@ -113,4 +113,15 @@ const totalSum = Number(input[2])
 let tempSum = 1000000000
 let max = Math.max(...requests)
 
-while (tempSum >= totalSum) {}
+while (tempSum >= totalSum) {
+  tempSum = requests.reduce((acc, cur) => {
+    if (cur > max) acc += max
+    else acc += cur
+
+    return acc
+  }, 0)
+
+  if (tempSum > totalSum) --max
+}
+
+console.log(Math.min(Math.max(...requests), max))
