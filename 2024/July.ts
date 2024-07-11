32907dd3415fc6c4d04040a534ea0542701f0be8
Seonmi Jung <devkatej927@gmail.com>
2024-07-11 16:01:04 +0900

    proceeding : baekjoon 2805. 나무 자르기

--- 2024/July.ts
+++ 2024/July.ts
@@ -5,8 +5,9 @@ const input240709 = fs240709.readFileSync('input.txt', 'utf8').trim().split('\n'
 const [treeCount, targetHeight] = input240709[0].split(' ').map(Number)
 const treeHeights = input240709[1].split(' ').map(Number)
 
-let start = 0
+let start = Math.min(...treeHeights)
 let end = Math.max(...treeHeights)
+let result
 
 while (start <= end) {
   const mid = Math.floor((start + end) / 2)
@@ -14,5 +15,16 @@ while (start <= end) {
   let restHeight = 0
 
   for (let curTreeHeight of treeHeights) {
+    const newRestHeight = mid <= curTreeHeight ? curTreeHeight - mid : 0
+    restHeight = +newRestHeight
+  }
+
+  if (restHeight === targetHeight) {
+    result = mid
+    break
+  } else if (restHeight < targetHeight) {
+    start = mid + 1
+  } else {
+    end = mid - 1
   }
 }
