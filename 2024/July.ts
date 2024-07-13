7fc8b808af83cbcce7c99a2a582511b330a26d7c
Seonmi Jung <devkatej927@gmail.com>
2024-07-13 09:50:53 +0900

    proceeding : baekjoon 2805. 나무 자르기

--- 2024/July.ts
+++ 2024/July.ts
@@ -1,9 +1,9 @@
 // ['24.7.9] baekjoon 2805. 나무 자르기
-const fs240709 = require('fs')
-const input240709 = fs240709.readFileSync('input.txt', 'utf8').trim().split('\n')
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n')
 
-const [treeCount, targetHeight] = input240709[0].split(' ').map(Number)
-const treeHeights = input240709[1].split(' ').map(Number)
+const [treeCount, targetHeight] = input[0].split(' ').map(Number)
+const treeHeights = input[1].split(' ').map(Number)
 
 let start = Math.min(...treeHeights)
 let end = Math.max(...treeHeights)
@@ -11,7 +11,6 @@ let result
 
 while (start <= end) {
   const mid = Math.floor((start + end) / 2)
-
   let restHeight = 0
 
   for (let curTreeHeight of treeHeights) {
@@ -19,12 +18,12 @@ while (start <= end) {
     restHeight += newRestHeight
   }
 
-  if (restHeight === targetHeight) {
+  if (targetHeight <= restHeight) {
     result = mid
-    break
-  } else if (restHeight < targetHeight) {
     start = mid + 1
   } else {
     end = mid - 1
   }
 }
+
+console.log(result)
